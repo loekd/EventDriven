5bbc40df5f336b69b9bd4ea88da463afc62bf589
Loek <admin@admin>
2017-10-05 13:13:10 +0200

    Add project files.

---
+++ EventReceiver/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace EventReceiver
+{
+    internal static class Program
+    {
+        private const string ServiceBusConnectionStringForReceive = "Endpoint=sb://centrictraining.servicebus.windows.net/;SharedAccessKeyName=Listen;SharedAccessKey=HXjuhKQzP2wV+bEKTjnZFo/V+fGTh4/0JpZEtvUUFA4=;";
+        private const string ServiceBusTopic = "TestTopic";
+        private const string ServiceBusSubscription = "TestSubscription";
+
+        private static SubscriptionClient _subscriptionClient;
+
+        static void Main()
+        {
+            Initialize();
+            while (true)
+            {
+                Console.WriteLine("Hit 1 to start listening.");
+                Console.WriteLine("Hit 2 to stop listening.");
+                Console.WriteLine("Hit escape to quit.");
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Initialize();
+
+                        break;
+                    case ConsoleKey.D2:
+                        Uninitialize();
+
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+
+                        break;
+                }
+            }
+        }
+
+        private static void Initialize()
+        {
+            if (_subscriptionClient != null)
+            {
+                return;
+            }
+
+            var client = new SubscriptionClient(ServiceBusConnectionStringForReceive, ServiceBusTopic, ServiceBusSubscription, ReceiveMode.ReceiveAndDelete);
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            client.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+            _subscriptionClient = client;
+
+
+        }
+
+        private static void Uninitialize()
+        {
+            _subscriptionClient?.CloseAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            _subscriptionClient = null;
+        }
+
+        private static Task ProcessMessageAsync(Message message, CancellationToken cancellationTokenc)
+        {
+            string payload = System.Text.Encoding.Unicode.GetString(message.Body);
+            //TODO: handle payload
+            Console.WriteLine($"Received message, id:{message.MessageId}, body:{payload}");
+            return Task.CompletedTask;
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs args)
+        {
+            Console.Error.WriteLine($"Received error:{args.Exception.Message}");
+            return Task.CompletedTask;
+        }
+
+    }
+
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Geospatial Location { get; set; }
+    }
+
+    internal class Geospatial
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+
+    }
+}
---
+++ EventSender/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Azure.ServiceBus;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+
+        private const string ServiceBusConnectionStringForSend = "Endpoint=sb://centrictraining.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=MD5qth4+3KgkaNYgVpIqsqoBwIEXdXpq7/r19Z7OzIM=;";
+        private const string ServiceBusTopic = "TestTopic";
+
+        private static TopicClient _topicClient;
+
+        static void Main()
+        {
+            Initialize();
+            while (true)
+            {
+                Console.WriteLine("Hit any key to send a message.");
+                Console.WriteLine("Hit escape to quit.");
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        SendMessage();
+                        break;
+                }
+            }
+        }
+
+        private static void Initialize()
+        {
+            var client = new TopicClient(ServiceBusConnectionStringForSend, ServiceBusTopic);
+            _topicClient = client;
+        }
+
+
+        private static void SendMessage()
+        {
+            //TODO: create meaningful payload
+            string message = "todo, send customer";
+            var brokeredMessage = new Message(System.Text.Encoding.Unicode.GetBytes(message));
+            _topicClient.SendAsync(brokeredMessage).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Geospatial Location { get; set; }
+    }
+
+    internal class Geospatial
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    internal class Event
+    {
+        public Guid Id { get; set; }
+        public int Sequence { get; set; }
+        public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
